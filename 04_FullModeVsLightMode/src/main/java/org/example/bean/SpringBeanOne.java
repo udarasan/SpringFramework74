61548c907beae1cbac50c323c098b8f71626d3ac
Udara San <udarassanjeewa@gmail.com>
2026-01-14 15:02:55 +0530

    update 2

--- 04_FullModeVsLightMode/src/main/java/org/example/bean/SpringBeanOne.java
+++ 04_FullModeVsLightMode/src/main/java/org/example/bean/SpringBeanOne.java
@@ -4,7 +4,6 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.stereotype.Component;
 
 
 public class SpringBeanOne implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
