2d39aed5e71a3dc680df8d0e98a14a05fbe273c0
Udara San <udarassanjeewa@gmail.com>
2026-01-14 15:28:07 +0530

    update 3

--- 04_FullModeVsLightMode/src/main/java/org/example/bean/SpringBeanThree.java
+++ 04_FullModeVsLightMode/src/main/java/org/example/bean/SpringBeanThree.java
@@ -1,7 +1,24 @@
 package org.example.bean;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBeanThree {
+    //Light Mode
+    @Bean
+    public SpringBeanOne springBeanOne() {
+        //Interbean dependency = not satisfied
+        //invoking bean method inside of a another bean method
+        SpringBeanTwo springBeanTwo1 = springBeanTwo();
+        SpringBeanTwo springBeanTwo2 = springBeanTwo();
+        System.out.println(springBeanTwo1);
+        System.out.println(springBeanTwo2);
+        return new SpringBeanOne();
+    }
+    @Bean
+    public SpringBeanTwo springBeanTwo() {
+        return new SpringBeanTwo();
+    }
+
 }
--- 04_FullModeVsLightMode/src/main/java/org/example/config/AppConfig.java
+++ 04_FullModeVsLightMode/src/main/java/org/example/config/AppConfig.java
@@ -1,10 +1,28 @@
 package org.example.config;
 
+import org.example.bean.SpringBeanOne;
+import org.example.bean.SpringBeanTwo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "org.example.bean")
 public class AppConfig {
+    //Full mode
+    /*@Bean
+    public SpringBeanOne springBeanOne() {
+        //interbean dependency - satisfied
+        SpringBeanTwo springBeanTwo1 = springBeanTwo();
+        SpringBeanTwo springBeanTwo2 = springBeanTwo();
+        System.out.println(springBeanTwo1);
+        System.out.println(springBeanTwo2);
+        return new SpringBeanOne();
+    }
+
+    @Bean
+    public SpringBeanTwo springBeanTwo() {
+        return new SpringBeanTwo();
+    }*/
 
 }
