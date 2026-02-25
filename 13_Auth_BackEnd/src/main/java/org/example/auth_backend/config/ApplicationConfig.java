c663cfd20bc0fcfe75b36ba682b7f4672569b472
Udara San <udarassanjeewa@gmail.com>
2026-02-25 14:29:19 +0530

    application config

---
+++ 13_Auth_BackEnd/src/main/java/org/example/auth_backend/config/ApplicationConfig.java
@@ -0,0 +1,37 @@
+package org.example.auth_backend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.example.auth_backend.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    private final UserRepository userRepository;
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByUsername(username)
+                .map(user -> new org.springframework.security.core.userdetails
+                        .User(
+                        user.getUsername(),
+                        user.getPassword(),
+                        List.of(new SimpleGrantedAuthority
+                                ("ROLE_"+user.getRole().name()))
+                )).orElseThrow(
+                        () -> new UsernameNotFoundException
+                                ("User not found with username: " + username)
+                );
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
