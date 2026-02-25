5147a2fb935eec25c31a8dff0fcb3e4d28d5b53e
Udara San <udarassanjeewa@gmail.com>
2026-02-25 13:14:39 +0530

    dto package

---
+++ 13_Auth_BackEnd/src/main/java/org/example/auth_backend/dto/AuthDTO.java
@@ -0,0 +1,9 @@
+package org.example.auth_backend.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthDTO {
+    private String username;
+    private String password;
+}
---
+++ 13_Auth_BackEnd/src/main/java/org/example/auth_backend/dto/RegisterDTO.java
@@ -0,0 +1,10 @@
+package org.example.auth_backend.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterDTO {
+    private String username;
+    private String password;
+    private String role;
+}
